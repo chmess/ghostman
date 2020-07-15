ee76222a1051f6e1b0e63561cb9556bbe32cf980
chmess <68294344+chmess@users.noreply.github.com>
2020-07-16 01:47:41 +0200

    Update cron.sh

--- webserver/scripts/cron.sh
+++ webserver/scripts/cron.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-GHOSTMAN_PATH=~/ghostman
-GHOST_PATH=~/ghostcore
+GHOSTMAN_PATH=/home/ghost/ghostman
+GHOST_PATH=/home/ghost/ghostcore
 HTML_PATH=$GHOSTMAN_PATH/webserver/public_html
 
-"$GHOSTMAN_PATH"/ghostman status > "$HTML_PATH"/ghostman-status.tmp
-"$GHOSTMAN_PATH"/ghostman stakingnode stats >> "$HTML_PATH"/ghostman-status.tmp
-"$GHOST_PATH"/ghost-cli getwalletinfo | grep watchonly >> "$HTML_PATH"/ghostman-status.tmp
+"$GHOSTMAN_PATH"/bin/ghostman.sh status |grep -v version |grep -v chainz > "$HTML_PATH"/ghostman-status.tmp
+"$GHOST_PATH"/ghost-cli getwalletinfo | grep watchonly |grep -v version >> "$HTML_PATH"/ghostman-status.tmp
+"$GHOSTMAN_PATH"/bin/ghostman.sh stakingnode stats |grep -v version >> "$HTML_PATH"/ghostman-status.tmp
